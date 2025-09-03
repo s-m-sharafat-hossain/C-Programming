5962327f96d489626b39808db6193a74b3261c59
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2025-09-03 10:51:50 +0600

    jj

---
+++ Basic/NewLine.c
@@ -0,0 +1,9 @@
+//New line using \n
+
+#include<stdio.h>
+
+int main(){
+    printf("Name: S M Sharafat Hossain\nID: 2312599642");
+    printf("\nDepartment: ECE");
+    return 0;
+}
---
+++ index.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello, Sharafat Hossain");
+    return 0;
+}
